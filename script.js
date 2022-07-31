c1fc3e5ffd99798e7d8cd87e77ffaa5f6b97e1f2
Zack Johnston <zackjohnston@me.com>
2022-07-31 10:34:09 -0500

    full functionality added for two human tic-tac-toe game

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ const Player = (name, mark) => {
 //object module for creating gameboard with event listener to run through gameplay functions
 const gameboard = (() => {
     const spots = document.querySelectorAll('.spot');
+
+    const endGame = () => {
+        spots.forEach((spot) => {spot.style.pointerEvents = 'none'});
+    };
+
     spots.forEach((spot) => {
         spot.addEventListener('click', () => {
             spot.textContent = game.getPlayerTurnMark();
-            game.checkForWinner();
+            spot.style.pointerEvents = 'none';
             game.checkForTie();
-            game.nextPlayer();
+            game.checkForWinner();
+            if (game.winnerDeclaredUpdate() || game.checkForTie()) {
+                endGame();
+            } else {game.nextPlayer()}
         });
     });
     return {spots};
@@ -23,9 +31,11 @@ const game = (() => {
     const player1 = Player('Player 1', 'X');
     const player2 = Player('Player 2', 'O');
     let playerTurn = player1;
+    let turnCount = 0;
+    let winnerDeclared = false;
 
     const winningCombo = [
-        [0,1,2],
+        [0,3,6],
         [1,4,7],
         [2,5,8],
         [0,1,2],
@@ -37,25 +47,36 @@ const game = (() => {
 
     const checkForWinner = () => {
         winningCombo.forEach((combo) => {
-            if ((gameboard.spots[combo[0]] && gameboard.spots[combo[1]] && gameboard.spots[combo[2]]) === playerTurn.mark) {
+            if (
+            gameboard.spots[combo[0]].textContent == playerTurn.mark &&
+            gameboard.spots[combo[1]].textContent == playerTurn.mark && 
+            gameboard.spots[combo[2]].textContent == playerTurn.mark) {
                 display.textContent = `${playerTurn.name} Wins`;
-                return true;
+                winnerDeclared = true;
             };
         });
     };
 
     const checkForTie = () => {
-        false
+        if (turnCount === 8) {
+            display.textContent = `The game is a tie`;
+            return true;
+        }    
     };
 
     const nextPlayer = () => {
         playerTurn === player1 ? playerTurn = player2 : playerTurn = player1;
         display.textContent = `${playerTurn.name} move`;
+        turnCount++;
     };
 
     const getPlayerTurnMark = () => {
         return playerTurn.mark;
     }
 
-    return {nextPlayer, getPlayerTurnMark, checkForWinner, checkForTie} 
+    const winnerDeclaredUpdate = () => {
+        return winnerDeclared;
+    }
+
+    return {nextPlayer, getPlayerTurnMark, checkForWinner, checkForTie, winnerDeclaredUpdate} 
 })();
